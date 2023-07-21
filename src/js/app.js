46d2fb09c097b1357dcb9483879bec7b0b4c0001
beksultan <hfjcgyrishwidhzhd1038@gmail.com>
2023-07-21 10:09:13 +0600

    3

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 const burgerBtn = document.querySelector(".header-mobile-nav__burger");
 const menu = document.querySelector(".header-mobile-menu");
 const closeButton = document.querySelector("#close-btn");
+const arr = []
 
 const menuToggle = () => {
     menu.classList.toggle("active");
